6ea91af5bac9c1978e513db8ceeb3d3b01f35599
ansangcuru <ansangcuru@gmail.com>
2025-01-10 09:24:05 +0900

    첫번째

---
+++ c702/src/main/java/kr/or/ysedu/c702/MessageController.java
@@ -0,0 +1,12 @@
+package kr.or.ysedu.c702;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MessageController {
+    @GetMapping("/message")
+    public String message(){
+        return "message";
+    }
+}
